e9a25f6295408be6f67a14021bf16a0a28ee456e
adi <kasaradi33@gmail.com>
2026-06-13 18:49:07 -0700

    Initial Commit

---
+++ CanvasBar/CanvasBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasBarApp.swift
+//  CanvasBar
+//
+//  Created by Adi Kasar on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct CanvasBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
